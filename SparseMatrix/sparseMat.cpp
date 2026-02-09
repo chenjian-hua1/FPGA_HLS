2efda6a41056de73acd6555d4b2c24be7d1d5d54
chenjian-hua1 <jianhua475@gmail.com>
2026-02-09 16:58:37 +0800

    modify

--- SparseMatrix/sparseMat.cpp
+++ SparseMatrix/sparseMat.cpp
@@ -34,46 +34,46 @@ void _coo_gemm(
     // 因為不知道每個 row 的非零元素量，不可能在電路runtime去計算nnz數量
     // 電路 runtime 階段才能知道要設計出多大電路 -> 很奇怪 電路不可能變形
     // 需要在編譯階段就知道 nnz 等資訊才能設計電路 (轉CSR)
-    int row_offset = 0;
-    int current_row = 0;
-
-    gemm: for (int i=0; i<SPARSE_MAT_NNZ; i++) {
-        if (current_row!=row_indices[i] || i==SPARSE_MAT_NNZ-1) {
-            // 最後一個 row 的上邊界在最後一個idx
-            bool is_row_end = (i == SPARSE_MAT_NNZ-1) || (row_indices[i+1] != row_indices[i]);
-            int end = is_row_end ? (i+1) : i;   // end 是「下一個 row_offset」
-            int nnz = end - row_offset;
-            
-            // Sparse Row 上的非零 col 索引, 數值
-            int row_nz_cols[DATA_ROWS];
-            MatType row_nz_values[DATA_ROWS];
-
-            for (int nz_idx=0; nz_idx<DATA_ROWS; nz_idx++) {
-			#pragma HLS UNROLL
-                row_nz_cols[nz_idx] = col_indices[row_offset+nz_idx];
-                row_nz_values[nz_idx] = values[row_offset+nz_idx];
-            }
-
-            // (1,nnz)mat * (nnz,DATA_COLS)mat
-            for (int c=0; c<DATA_COLS; c++) {
-			#pragma HLS UNROLL
-                // 將內積不是對到 0 的部分取出來
-                MatType col_elements[DATA_ROWS] = {0};
-                for (int nz_idx=0; nz_idx<DATA_ROWS; nz_idx++) {
-				#pragma HLS UNROLL
-                	col_elements[nz_idx] = data[row_nz_cols[nz_idx]][c];
-                }
-                
-                _inner_product<DATA_ROWS>(row_nz_values,col_elements,out[current_row][c]);
-            }
-
-            row_offset = i;
-            current_row = row_indices[i];
-        }
-        else {
-            continue;
-        }
-    }
+//    int row_offset = 0;
+//    int current_row = 0;
+//
+//    gemm: for (int i=0; i<SPARSE_MAT_NNZ; i++) {
+//        if (current_row!=row_indices[i] || i==SPARSE_MAT_NNZ-1) {
+//            // 最後一個 row 的上邊界在最後一個idx
+//            bool is_row_end = (i == SPARSE_MAT_NNZ-1) || (row_indices[i+1] != row_indices[i]);
+//            int end = is_row_end ? (i+1) : i;   // end 是「下一個 row_offset」
+//            int nnz = end - row_offset;
+//
+//            // Sparse Row 上的非零 col 索引, 數值
+//            int row_nz_cols[DATA_ROWS];
+//            MatType row_nz_values[DATA_ROWS];
+//
+//            for (int nz_idx=0; nz_idx<DATA_ROWS; nz_idx++) {
+//			#pragma HLS UNROLL
+//                row_nz_cols[nz_idx] = col_indices[row_offset+nz_idx];
+//                row_nz_values[nz_idx] = values[row_offset+nz_idx];
+//            }
+//
+//            // (1,nnz)mat * (nnz,DATA_COLS)mat
+//            for (int c=0; c<DATA_COLS; c++) {
+//			#pragma HLS UNROLL
+//                // 將內積不是對到 0 的部分取出來
+//                MatType col_elements[DATA_ROWS] = {0};
+//                for (int nz_idx=0; nz_idx<DATA_ROWS; nz_idx++) {
+//				#pragma HLS UNROLL
+//                	col_elements[nz_idx] = data[row_nz_cols[nz_idx]][c];
+//                }
+//
+//                _inner_product<DATA_ROWS>(row_nz_values,col_elements,out[current_row][c]);
+//            }
+//
+//            row_offset = i;
+//            current_row = row_indices[i];
+//        }
+//        else {
+//            continue;
+//        }
+//    }
 }
 
 // CSR (Coordinate) format : save nonzero row, col indices 
@@ -88,10 +88,10 @@ void _csr_gemm(
     csr_gemm: for (int r=0; r<SPARSE_ROWS; r++) {
     #pragma HLS UNROLL
 
-        int row_start = row_offset[r];
-        int row_end = row_offset[r+1];
+        constexpr int row_start = row_offset[r];
+        constexpr int row_end = row_offset[r+1];
 
-        const int nnz = row_end-row_start;
+        constexpr int nnz = row_end-row_start;
         MatType row_nz_values[nnz];
         MatType row_nz_col_idx[nnz];
 
@@ -159,9 +159,9 @@ void sparse_gemm(MatType *data_ptr, MatType *out) {
 }
 
 
-int main() {
-    MatType *data, out[A_ROWS*A_COLS];
-    sparse_gemm(data, out);
-    std::cout << "hello world" << std::endl;
-    return 0;
-}
+//int main() {
+//    MatType *data, out[A_ROWS*A_COLS];
+//    sparse_gemm(data, out);
+//    std::cout << "hello world" << std::endl;
+//    return 0;
+//}
