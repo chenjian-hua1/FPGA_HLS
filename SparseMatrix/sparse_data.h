e30ac842d70b0b100f952a295cc13fbc59642056
chenjian-hua1 <jianhua475@gmail.com>
2026-02-09 12:06:50 +0800

    add csr data

--- SparseMatrix/sparse_data.h
+++ SparseMatrix/sparse_data.h
@@ -5,22 +5,24 @@
 
 #define A_COLS 5
 
-#define A_NNZ 9
+#define A_NNZ 10
 
 #define A_ROWS 5
 
-static const int32_t A_col_idx[9] = {1, 4, 1, 3, 0, 2, 0, 2, 4};
+static const int32_t A_col_idx[10] = {0, 4, 0, 2, 0, 3, 4, 1, 3, 4};
 
 static const int32_t A_dense[5][5] = {
-  {0, 3, 0, 0, 2},
-  {0, 3, 0, 7, 0},
-  {7, 0, 2, 0, 0},
-  {3, 0, 0, 0, 0},
-  {0, 0, 4, 0, 4}
+  {3, 0, 0, 0, 1},
+  {4, 0, 3, 0, 0},
+  {5, 0, 0, 4, 1},
+  {0, 0, 0, 0, 0},
+  {0, 6, 0, 4, 4}
 };
 
-static const int32_t A_row_idx[9] = {0, 0, 1, 1, 2, 2, 3, 4, 4};
+static const int32_t A_row_idx[10] = {0, 0, 1, 1, 2, 2, 2, 4, 4, 4};
 
-static const int32_t A_values[9] = {3, 2, 3, 7, 7, 2, 3, 4, 4};
+static const int32_t A_row_ptr[6] = {0, 2, 4, 7, 7, 10};
+
+static const int32_t A_values[10] = {3, 1, 4, 3, 5, 4, 1, 6, 4, 4};
 
 #endif // SPARSE_DATA_H_
