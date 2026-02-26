3f37540659ce2d901f3268d8029e9a3f6a2e0a15
chenjian-hua <yourgitemail@example.com>
2026-02-26 12:56:38 +0800

    update headfile

--- SparseMatrix/sparseMat.h
+++ SparseMatrix/sparseMat.h
@@ -55,12 +55,20 @@ typedef ap_int<32> matType;
 #define FMT_SELL  2
 #define FMT_BSR   3
 #define FMT_ELL   4
+
 // ...之後要加就一直加
+enum FMT {
+    COO,
+    CSR,
+    SELL,
+    BSR,
+    ELL
+};
 
 // 最後用這個宏指定「本次要用哪個」
 // 預設值（也可以不給，強制外部一定要定義）
 #ifndef STORAGE_FMT_ID
-#define STORAGE_FMT_ID FMT_CSR
+FMT STORAGE_FMT_ID = CSR;
 #endif
 
 
